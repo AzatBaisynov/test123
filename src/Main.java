04c66e201aaf7a56b07b96fbe36f172c7ea00b9c
AzatBaisynov <azat.baisynov@gmail.com>
2024-07-24 20:19:42 +0600

    fix: System.out

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@
 public class Main {
     public static void main(String[] args) {
        Shtativ shtativ = new Shtativ();
-       ShtativPhoneable shtativ2 = new Shtativ();
+
+
+        System.out.println("HELLO WORLD");
 
     }
 }
